f0b6d4b01cd2276ac36966035de0ae2a63d76112
vinhnx <nxvinh222@gmail.com>
2021-11-30 16:39:30 +0700

    Add express API

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express'); // Adding Express
+var timeout = require('connect-timeout');
+const puppeteer = require("puppeteer");
+const scrapeText = require('./utils/scrapeText.js')
+
+const app = express(); // Initializing Express
+
+app.use(timeout('300s'));
+app.use(haltOnTimedout);
+app.use(express.json())
+
+// Wrapping the Puppeteer browser logic in a GET request
+app.post('/', async function (req, res) {
+    const result = await scrapeText(req.body)
+    res.send(result)
+});
+
+// Making Express listen on port 7000
+app.listen(7000, function () {
+    console.log('Running on port 7000.');
+});
+
+function haltOnTimedout(req, res, next) {
+    if (!req.timedout) next();
+}
---
+++ server/utils/scrapeText.js
@@ -0,0 +1,49 @@
+const puppeteer = require("puppeteer");
+
+async function scrape(request) {
+    // const request = {
+    //     url: "https://vnexpress.net/",
+    //     elements: [
+    //         {
+    //             name: "title",
+    //             selector: "article:nth-of-type(n+7) .title-news a",
+    //             type: ""
+    //         },
+    //         {
+    //             name: "content",
+    //             selector: "article:nth-of-type(n+2) .description a[data-thumb='1']",
+    //             type: ""
+    //         }
+    //     ]
+    // }
+    const browser = await puppeteer.launch({ headless: true })
+    const page = await browser.newPage()
+    let crawlResult = []
+    await page.goto(request.url, { waitUtil: "networkkidle0" })
+
+    await Promise.all(request.elements.map(async (element) => {
+        let crawledElementsContent = await page.evaluate((element) => {
+            let crawledElementsContent = []
+            console.log("element: ");
+            console.log(element)
+            let crawledElements = document.querySelectorAll(element.selector)
+            crawledElements.forEach((crawledElement) => {
+                crawledElementsContent.push(crawledElement.innerText)
+            })
+            return {
+                [element.name]: crawledElementsContent
+            }
+        }, element)
+        crawlResult.push(crawledElementsContent)
+    }
+    ))
+
+    // console.log(crawlResult);
+    await browser.close()
+
+    return crawlResult
+}
+
+
+module.exports = scrape;
+
