57fb4b7db8818c39f119324412ce4ad2d3a8aa31
Vinh <nxvinh222@gmail.com>
2022-04-29 16:38:05 +0700

    Update timeout

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
-const express = require('express'); // Adding Express
-var timeout = require('connect-timeout');
+const express = require("express"); // Adding Express
+var timeout = require("connect-timeout");
 const puppeteer = require("puppeteer");
-const fs = require('fs');
+const fs = require("fs");
 
-const simpleCrawlTransport = require('./api/transport/simpleCrawl.js');
-const advanceCrawlTransport = require('./api/transport/advanceCrawl.js');
+const simpleCrawlTransport = require("./api/transport/simpleCrawl.js");
+const advanceCrawlTransport = require("./api/transport/advanceCrawl.js");
 
 process.setMaxListeners(Infinity);
 
 const app = express(); // Initializing Express
 
-app.use(timeout('1200s'));
+app.use(timeout("1200s"));
 app.use(haltOnTimedout);
-app.use(express.json())
+app.use(express.json());
 
 // Wrapping the Puppeteer browser logic in a GET request
-app.post('/simple', simpleCrawlTransport);
-app.post('/advance', advanceCrawlTransport);
+app.post("/simple", simpleCrawlTransport);
+app.post("/advance", advanceCrawlTransport);
 
 // Making Express listen on port 7000
 app.listen(7000, function () {
-    console.log('Running on port 7000.');
+  console.log("Running on port 7000.");
 });
 
 function haltOnTimedout(req, res, next) {
-    if (!req.timedout) next();
+  if (!req.timedout) next();
 }
