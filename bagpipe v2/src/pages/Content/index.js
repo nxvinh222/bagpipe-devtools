3415e6851621bab67bbf2ad6fecfb3a73e920b7c
vinhnx <nxvinh222@gmail.com>
2022-06-08 15:19:03 +0700

    Inject jQuery

--- bagpipe v2/src/pages/Content/index.js
+++ bagpipe v2/src/pages/Content/index.js
@@ -10,13 +10,22 @@ var current_attr_index = attr_index
 var current_element = "null"
 var final_element = "null"
 var result_demo = {}
-var extension_element = ["select-panel", "attr-con", "attr-input", "bp-add-button", "bp-confirm-button", "bagpipe-finish", "bagpipe-scrape-inject"]
+var extension_element = ["select-panel", "attr-con", "attr-input", "bp-add-button", "bp-confirm-button", "bagpipe-finish", "bagpipe-scrape-inject", "bagpipe-instruction"]
 
 console.log('Content script works!!');
 console.log('Must reload extension for modifications to take effect.');
-printLine("Using the 'printLine' function from the Print Module");
 
+// Inject jQuery
+const contentScriptLinks = ['bagpipe-jquery.min.js'];
+contentScriptLinks.forEach(src => {
+    let el = document.createElement('script');
+    el.src = chrome.runtime.getURL(src);
+    document.body.appendChild(el);
+});
+
+//Inject Tool bar
 $.get(chrome.runtime.getURL('./tool.html'), function (data) {
+    // Append to body
     $(data).appendTo('body');
     console.log('injected')
     chrome.storage.sync.set({ "elements": "Selecting........." }, function () {
