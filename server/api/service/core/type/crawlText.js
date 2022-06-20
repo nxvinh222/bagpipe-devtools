754629ef6fa6a3bd11c0fbf573da22b0fb49f5d6
vinhnx <nxvinh222@gmail.com>
2022-06-21 02:35:55 +0700

    Edit regex

--- server/api/service/core/type/crawlText.js
+++ server/api/service/core/type/crawlText.js
@@ -5,10 +5,14 @@ const crawlText = (childElement) => {
     debugger;
     console.log(childElement.selector);
     crawledElements.forEach((crawledElement, index) => {
-        let crawledText = crawledElement.innerText.replace(/[\\]/g, '');
-        // let crawledText = crawledElement.innerText.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
-        crawledText = crawledText.replaceAll("\n", "\\n")
-        crawledText = crawledText.replaceAll("\t", "")
+        let crawledText = crawledElement.innerText
+        // let crawledText = crawledElement.innerText.replace(/[\\]/g, '');
+        // crawledText = crawledText.replaceAll("\n", "\\\\n");
+        crawledText = crawledText.replaceAll("\n", " ");
+        crawledText = crawledText.replaceAll("\t", " ");
+        // crawledText = crawledText.replace("/\\/g", "\\\\");
+        crawledText = crawledText.replace("/\\/g", " ");
+        // crawledText = crawledText.replace(/[&\/\\#,+()$~%.'":;*?<>{}]/g, '');
 
         crawledElementsContent.push(crawledText)
     })
